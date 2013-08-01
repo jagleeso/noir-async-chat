05c5eab6eb7df849d695698433f73dc700e284e1
James Gleeson <chocomoojuice@gmail.com>
2013-08-01 08:27:02 -0400

    update dependencies to newer versions (older versions missing from maven repos)

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
 (defproject noir-async-chat "1.0.0-SNAPSHOT"
   :main noir-async-chat.server
   :description "An example using noir-async"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir-async "1.0.0-SNAPSHOT"]
-                 [cheshire "2.0.4"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [noir "1.3.0-beta3"]
+                 [noir-async "1.1.0-beta11"]])
