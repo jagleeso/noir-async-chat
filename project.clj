11384c402c126dccbb0798a3f0302fd99764d930
James <jagleeso@gmail.com>
2013-08-10 17:18:33 -0400

    add dependency for mp3 tag reading

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-
 (defproject noir-async-chat "1.0.0-SNAPSHOT"
   :main noir-async-chat.server
   :description "An example using noir-async"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [noir "1.3.0-beta3"]
-                 [noir-async "1.1.0-beta11"]])
+                 [noir-async "1.1.0-beta11"]
+                 [claudio "0.1.2"]])
